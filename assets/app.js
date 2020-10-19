6d1d776b238074c6e299a50b46bb50a57fda5679
desbma <desbma@users.noreply.github.com>
2020-10-20 01:13:39 +0200

    Initial JS

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,20 @@
 $(function() {
-  console.log("hey");
+  // click event: wrap in form input
+  $("td.str").click(function() {
+    if ($(this).find("input").length > 0) {
+      return true;
+    }
+
+    var inner_val = $(this).text();
+    var wrapped_val = $("<input type=\"text\"/>").val(inner_val);
+    $(this).html(wrapped_val);
+    return false;
+  });
+
+  // input unfocus
+  $("body").on("blur", "td.str input", function() {
+    var inner_val = $(this).val();
+    $(this).replaceWith(inner_val);
+    return true;
+  });
 });
