2c7747e67fd93d833ce0175ebcb56696c4086f1f
desbma <desbma@users.noreply.github.com>
2020-10-20 01:14:07 +0200

    fmt

--- src/main.rs
+++ src/main.rs
@@ -118,8 +118,14 @@ fn rocket() -> rocket::Rocket {
 
 fn main() {
     simple_logger::SimpleLogger::new()
-        .with_module_level("hyper", std::cmp::min(log::LevelFilter::Info, log::max_level()))
-        .with_module_level("mio::poll", std::cmp::min(log::LevelFilter::Info, log::max_level()))
+        .with_module_level(
+            "hyper",
+            std::cmp::min(log::LevelFilter::Info, log::max_level()),
+        )
+        .with_module_level(
+            "mio::poll",
+            std::cmp::min(log::LevelFilter::Info, log::max_level()),
+        )
         .with_module_level("_", std::cmp::min(log::LevelFilter::Info, log::max_level()))
         .init()
         .unwrap();
